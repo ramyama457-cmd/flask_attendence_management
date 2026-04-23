e557c52d695d91e4d55acb18fac1ef81a506b7bf
ramya manjunath angadi <ramyama457@gmail.com>
2026-04-23 10:07:08 +0530

    wcwc

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, session
 from flask import request
 from model import db, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///site.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATION']=False
-
-session={}
+app.secret_key = 'your_secret_key_here'
 
 db.init_app(app)
 with app.app_context():
@@ -14,48 +13,52 @@ with app.app_context():
 
 @app.route("/")
 def hello_world():
-    return render_template("home.html")
+    user = User.query.get(session.get('user_id')) if session.get('user_id') else None
+    return render_template("home.html", user=user)
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    user = User.query.get(session.get('user_id')) if session.get('user_id') else None
+    return render_template("about.html", user=user)
 
 @app.route("/signup",methods=["POST","GET"])
 def signup():
+    user = User.query.get(session.get('user_id')) if session.get('user_id') else None
     if request.method == "POST":
         name = request.form.get("name")
         email = request.form.get("email")
         password = request.form.get("password")
         db_user=User.query.filter_by(email=email).first()
         if db_user:
-            return render_template("Signup.html",error="Email already exists")
+            return render_template("Signup.html",error="Email already exists", user=user)
         db_user=User.query.filter_by(name=name).first()
         if db_user:
-            return render_template("Signup.html",error="Name already exists")
+            return render_template("Signup.html",error="Name already exists", user=user)
         user=User(name=name,email=email,password=password)
         db.session.add(user)
         db.session.commit()
-        return render_template("Login.html")    
-    return render_template("Signup.html")
+        return render_template("Login.html", user=user)    
+    return render_template("Signup.html", user=user)
 
         
 
 @app.route("/login",methods=["POST","GET"])
 def login():
+    user = User.query.get(session.get('user_id')) if session.get('user_id') else None
     if request.method == "POST":
         email = request.form.get("email")
         password = request.form.get("password")
-        user=User.query.filter_by(email=email,password=password).first()
-        if user:
-            session["user.id"]=user
-            return render_template("home.html",user=user)
+        db_user=User.query.filter_by(email=email,password=password).first()
+        if db_user:
+            session["user_id"]=db_user.id
+            return render_template("home.html",user=db_user)
         else:
-            return render_template("login.html")
-    return render_template("login.html")
+            return render_template("login.html", user=user)
+    return render_template("login.html", user=user)
 
-@app.route("/logout/<int:user_id>")
-def logout(user_id):
-    session.pop(user_id,None)
+@app.route("/logout")
+def logout():
+    session.pop('user_id', None)
     return render_template("home.html",user=None)
 
 
